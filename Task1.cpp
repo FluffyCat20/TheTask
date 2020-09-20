05a197c50cf62f9efde47136f73fdbcf7ca56595
FluffyCat20 <71210269+FluffyCat20@users.noreply.github.com>
2020-09-20 14:23:55 +0300

    second commit

--- Task1.cpp
+++ Task1.cpp
@@ -21,7 +21,7 @@ void make_white(int i, int j)
 
 int main()
 {
-	int r = 16; //радиус окрестности, в которой высчитывается формула
+	int r = 200; //радиус окрестности, в которой высчитывается формула
 	double k = -0.2; //эмпирически определяемый параметр
 	double nAverage, average, sqrnDeviation, threshold;
 	img = imread("../test3.png", IMREAD_COLOR);
@@ -32,68 +32,67 @@ int main()
 	}
 	int Isize = img.rows;
 	int Jsize = img.cols;
-	int Isize1 = Isize + 2 * r;
-	int Jsize1 = Jsize + 2 * r;
-	int n = (2 * r + 1) * (2 * r + 1);
+
 	cvtColor(img, img, COLOR_BGR2GRAY);
 
-	double** brightness = new double* [Isize1];
-	for (int i = 0; i < Isize1; i++)
-		brightness[i] = new double[Jsize1];
-	for (int i = 0; i < Isize1; i++)
-		for (int j = 0; j < Jsize1; j++)
-		{
-			brightness[i][j] = -1;
-		}
-
+	double** brightness = new double* [Isize];
+	for (int i = 0; i < Isize; i++)
+		brightness[i] = new double[Jsize];
+	
 	for (int i = 0; i < Isize; i++)
 		for (int j = 0; j < Jsize; j++)
 		{
-			brightness[i + r][j + r] = img.at<uchar>(i, j);
+			brightness[i][j] = img.at<uchar>(i, j);
 		}
 
-	//(i,r) first
-	for (int i = r; i < Isize + r; i++)
+	//(i, 0) first
+	for (int i = 0; i < Isize; i++)
 	{
+		int n = (min (Isize, i+r+1) - max (0,i-r)) * (r+1);
 		average = nAverage = sqrnDeviation = 0;
-		for (int i1 = i - r; i1 < i + r + 1; i1++)
-			for (int j1 = r; j1 < 2 * r + 1; j1++)
-				if (brightness[i1][j1] != -1) nAverage += brightness[i1][j1];
+		for (int i1 = max(0, i - r); i1 < min(Isize, i + r + 1); i1++)
+			for (int j1 = 0; j1 < r+1; j1++)
+				nAverage += brightness[i1][j1];
 		average = nAverage / n;
-		for (int i1 = i - r; i1 < i + r + 1; i1++)
-			for (int j1 = r; j1 < 2 * r + 1; j1++)
+		for (int i1 = max(0, i - r); i1 < min(Isize, i + r + 1); i1++)
+			for (int j1 = 0; j1 < r + 1; j1++)
 			{
 				double b = brightness[i1][j1];
-				if (b != -1) sqrnDeviation += (b - average) * (b - average);
+				sqrnDeviation += (b - average) * (b - average);
 				//sqrnDeviation += b * b;
 			}
 		threshold = average + k * sqrt(sqrnDeviation / n);
 		//threshold = average + k * sqrt(-average * average + sqrnDeviation / n);
-		if (brightness[i][r] < threshold) make_black(i - r, 0); else make_white(i - r, 0);
+		if (brightness[i][0] < threshold) make_black(i, 0); else make_white(i, 0);
 
-		for (int j = r + 1; j < Jsize + r; j++)
+		for (int j = 1; j < Jsize; j++)
 		{
-			for (int k = i - r; k < i + r + 1; k++)
+			n = (min(Jsize, j+r) - max(0, j-r))*(min(Isize, i+r)-max(0, i-r));
+			for (int k1 = i - r; k1 < i + r + 1; k1++)
 			{
-				double b1 = brightness[k][j - r - 1];
-				double b2 = brightness[k][j + r];
-				if (b1 != -1) nAverage -= b1;
-				if (b2 != -1) nAverage += b2;
+				double b1 = 0;
+				if (j-r-1>=0 && k1>=0 && k1 < Isize) b1 = brightness[k1][j - r - 1];
+				double b2 = 0;
+				if (j+r<Jsize && k1 >= 0 && k1<Isize) b2 = brightness[k1][j + r];
+				nAverage -= b1;
+				nAverage += b2;
 			}
 			average = nAverage / n;
-			for (int k = i - r; k < i + r + 1; k++)
+			for (int k1 = i - r; k1 < i + r + 1; k1++)
 			{
-				double b1 = brightness[k][j - r - 1];
-				double b2 = brightness[k][j + r];
+				double b1 = average;
+				if (j - r - 1 >= 0 && k1 >= 0 && k1 < Isize) b1 = brightness[k1][j - r - 1];
+				double b2 = average;
+				if (j + r < Jsize && k1 >= 0 && k1 < Isize) b2 = brightness[k1][j + r];
 				//sqrnDeviation -= b1 * b1;
 				//sqrnDeviation += b2 * b2;
-				if (b1 != -1) sqrnDeviation -= (b1 - average) * (b1 - average);
-				if (b2 != -1) sqrnDeviation += (b2 - average) * (b2 - average);
+				sqrnDeviation -= (b1 - average) * (b1 - average);
+				sqrnDeviation += (b2 - average) * (b2 - average);
 			}
 
 			threshold = average + k * sqrt(sqrnDeviation / n);
 			//threshold = average + k * sqrt(-average * average + sqrnDeviation / n);
-			if (brightness[i][j] < threshold) make_black(i - r, j - r); else make_white(i - r, j - r);
+			if (brightness[i][j] < threshold) make_black(i, j); else make_white(i, j);
 		}
 	}
 
@@ -102,7 +101,7 @@ int main()
 	waitKey(0);
 
 	destroyWindow("window");
-	for (int i = 0; i < Isize1; i++)
+	for (int i = 0; i < Isize; i++)
 	{
 		delete[] brightness[i];
 	}
