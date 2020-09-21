021991e2ba61f9361a8be136e5b394e3c7171186
FluffyCat20 <71210269+FluffyCat20@users.noreply.github.com>
2020-09-21 12:15:56 +0300

    algorithm as function

--- Task1.cpp
+++ Task1.cpp
@@ -8,6 +8,9 @@ using namespace cv;
 using namespace std;
 
 Mat img;
+int Isize, Jsize;
+int r = 200; //радиус окрестности, в которой высчитывается формула
+double k = -0.2; //эмпирически определяемый параметр
 
 void make_black(int i, int j)
 {
@@ -19,39 +22,16 @@ void make_white(int i, int j)
 	img.at<uchar>(i, j) = 255;
 }
 
-int main()
+void niblack_algorithm(double** brightness)
 {
-	int r = 200; //радиус окрестности, в которой высчитывается формула
-	double k = -0.2; //эмпирически определяемый параметр
 	double nAverage, average, sqrnDeviation, threshold;
-	img = imread("../test3.png", IMREAD_COLOR);
-	if (img.empty())
-	{
-		std::cout << "error" << endl;
-		return -1;
-	}
-	int Isize = img.rows;
-	int Jsize = img.cols;
-
-	cvtColor(img, img, COLOR_BGR2GRAY);
-
-	double** brightness = new double* [Isize];
-	for (int i = 0; i < Isize; i++)
-		brightness[i] = new double[Jsize];
-	
-	for (int i = 0; i < Isize; i++)
-		for (int j = 0; j < Jsize; j++)
-		{
-			brightness[i][j] = img.at<uchar>(i, j);
-		}
-
 	//(i, 0) first
 	for (int i = 0; i < Isize; i++)
 	{
-		int n = (min (Isize, i+r+1) - max (0,i-r)) * (r+1);
+		int n = (min(Isize, i + r + 1) - max(0, i - r)) * (r + 1);
 		average = nAverage = sqrnDeviation = 0;
 		for (int i1 = max(0, i - r); i1 < min(Isize, i + r + 1); i1++)
-			for (int j1 = 0; j1 < r+1; j1++)
+			for (int j1 = 0; j1 < r + 1; j1++)
 				nAverage += brightness[i1][j1];
 		average = nAverage / n;
 		for (int i1 = max(0, i - r); i1 < min(Isize, i + r + 1); i1++)
@@ -67,13 +47,13 @@ int main()
 
 		for (int j = 1; j < Jsize; j++)
 		{
-			n = (min(Jsize, j+r) - max(0, j-r))*(min(Isize, i+r)-max(0, i-r));
+			n = (min(Jsize, j + r) - max(0, j - r)) * (min(Isize, i + r) - max(0, i - r));
 			for (int k1 = i - r; k1 < i + r + 1; k1++)
 			{
 				double b1 = 0;
-				if (j-r-1>=0 && k1>=0 && k1 < Isize) b1 = brightness[k1][j - r - 1];
+				if (j - r - 1 >= 0 && k1 >= 0 && k1 < Isize) b1 = brightness[k1][j - r - 1];
 				double b2 = 0;
-				if (j+r<Jsize && k1 >= 0 && k1<Isize) b2 = brightness[k1][j + r];
+				if (j + r < Jsize && k1 >= 0 && k1 < Isize) b2 = brightness[k1][j + r];
 				nAverage -= b1;
 				nAverage += b2;
 			}
@@ -95,6 +75,32 @@ int main()
 			if (brightness[i][j] < threshold) make_black(i, j); else make_white(i, j);
 		}
 	}
+}
+
+int main()
+{
+	img = imread("../test1.png", IMREAD_COLOR);
+	if (img.empty())
+	{
+		std::cout << "error" << endl;
+		return -1;
+	}
+	Isize = img.rows;
+	Jsize = img.cols;
+
+	cvtColor(img, img, COLOR_BGR2GRAY);
+
+	double** brightness = new double* [Isize];
+	for (int i = 0; i < Isize; i++)
+		brightness[i] = new double[Jsize];
+	
+	for (int i = 0; i < Isize; i++)
+		for (int j = 0; j < Jsize; j++)
+		{
+			brightness[i][j] = img.at<uchar>(i, j);
+		}
+
+	niblack_algorithm(brightness);
 
 	namedWindow("window", WINDOW_AUTOSIZE);
 	imshow("window", img);
